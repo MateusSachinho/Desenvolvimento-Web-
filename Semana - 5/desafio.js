766af26977da5cca5aab976a94d55c6b5d853cdd
MATEUS DE MACEDO COELHO SACHINHO <202403184672@alunos.ibmec.edu.br>
2024-09-16 10:59:34 -0300

    semana 5 desafio

---
+++ Semana - 5/desafio.js
@@ -0,0 +1,20 @@
+function func1(x, y){
+    return x + y;
+  }
+  
+  const func2 = function (a, b){
+    return a*b; 
+  }
+  
+  const func3 = entrada => entrada+' é bonito';
+  
+  function func4(m){
+    console.log(m+1);
+  } 
+  
+  const coisa = [25, 'casa', [34,12], 9.65];
+
+
+
+
+//coisa.forEach( (m) => func4(m) );
